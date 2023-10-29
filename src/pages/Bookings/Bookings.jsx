8ca78b27079c16b9e8784789f2ee913acc52c6c1
Programming Hero <programming.hero1@gmail.com>
2023-10-28 19:09:34 -0500

    handle cookies for login and logout

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -8,7 +8,7 @@ const Bookings = () => {
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
-        fetch(url)
+        fetch(url, {credentials: 'include'})
             .then(res => res.json())
             .then(data => setBookings(data))
     }, [url]);
